dec3e6af5160df6ec3f55d01e1ed4142e5f7bd55
mraduldubey <mraduldubeymd19@gmail.com>
2017-02-27 16:23:45 +0530

    Admin can modify models

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import dashboard
+
+class dashboardAdmin(admin.ModelAdmin):
+	class Meta:
+		model = dashboard
+
+admin.site.register(dashboard,dashboardAdmin)
--- dashboard/models.py
+++ dashboard/models.py
@@ -4,8 +4,10 @@ from django.db import models
 
 # Create your models here.
 class dashboard(models.Model):
-	username = models.CharField(max_length=100)
+	username = models.CharField(max_length = 100)
 	about = models.TextField(default = 'default about text')
-	def __unicode__(self):
-		return self.name
+	location = models.CharField(default="location default text",max_length = 200,blank=True)
+	worktitle = models.CharField(max_length=100,null=True,blank=True)
+	def __unicode__(self):  #used to display username on admin pages. 
+		return self.username
 
